26e6018a7105fd30d187a4474d9ec53af4d07bf4
sennuraldemir <99283497+sennuraldemir@users.noreply.github.com>
2022-10-07 14:16:13 +0300

    Update odev1.py

--- odev1.py
+++ odev1.py
@@ -1,4 +1,6 @@
-name='sennur'
-surname='aldemir'
 
-print('My name is'+name+'surname'+surname)
+//Girilen 2 Sayıyı Toplayan Python Örneği
+sayi1 = input('1. Sayı : ')
+sayi2 = input('1. Sayı : ')
+toplam=float(sayi1)+float(sayi2)
+print("Toplam :{0} ".format(toplam))
